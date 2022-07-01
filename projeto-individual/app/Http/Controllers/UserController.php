35dadbcdaabc1415e9b69ef2bdc77f0cf005fbd2
Victor Feitoza <84550540+fTz997@users.noreply.github.com>
2022-07-01 19:05:12 -0300

    POST login form

--- projeto-individual/app/Http/Controllers/UserController.php
+++ projeto-individual/app/Http/Controllers/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Illuminate\Http\Request;
 
 
 class UserController extends Controller
@@ -27,4 +28,12 @@ class UserController extends Controller
   {
     return view('users.create');
   }
+
+  public function store(Request $request)
+  {
+    dd($request->all());
+  }
+
+
+
 }
--- projeto-individual/resources/views/users/create.blade.php
+++ projeto-individual/resources/views/users/create.blade.php
@@ -1,1 +1,22 @@
-<h1>teste</h1>
+@extends('template.users')
+@section('title', "Cadastrar usuário")
+@section('body')
+
+<h1>Novo Usuário</h1>
+<form action="{{route('users.store')}}" method="POST">
+  @csrf
+  <div>
+    <label for="name" class="form-label"> Seu nome</label>
+    <input type="text" class="form-control" id="name" name="name" aria-describedby="Nome">
+  </div>
+  <div class="mb-3">
+    <label for="email" class="form-label">Seu email</label>
+    <input type="email" class="form-control" id="email" name="email">
+  </div>
+  <div class="mb-3">
+    <label for="password" class="form-label">Seu senha</label>
+    <input type="password" class="form-control" id="password" name="passwodr">
+  </div>
+  <button type="submit" class="btn btn-primary">Enviar</button>
+</form>
+@endsection
